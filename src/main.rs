e8aa68644af725f141491913ba4c7f4702a7c59d
Nikshiqi <nikolas.nikshiqi@landmark.al>
2026-01-20 11:18:28 +0100

    public repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 
-use image::imageops::FilterType;
+use image::{RgbaImage, imageops::FilterType};
 use termsize::Size;
 
 struct Args {
@@ -17,13 +17,12 @@ fn main() {
         path: cmd[1].to_string()
     };
 
-
     let mut output = String::new();
-    let asci_values: [char; 10] = ['&', '$', 'X', 'x', '=', '+', ';', ':', '.', ' '];
+    let asci_values: [char; 9] = ['@','&', '$', 'x', '=', '+', ':', '.', ' '];
     let img = image::open(args.path).unwrap();
     let gray_img = img.grayscale();
 
-
+    //Get terminal dimensions
     let (term_height, term_width) = match termsize::get() {
         Some(Size { rows, cols }) => {
             println!("{} {}",rows,cols);
@@ -33,16 +32,16 @@ fn main() {
     };
 
     let resized_gimage = gray_img.resize_exact(term_width , term_height, FilterType::Nearest);
-    let buffer: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> = resized_gimage.to_rgba8();
+    let buffer: RgbaImage = resized_gimage.to_rgba8();
 
-    for (row_index, row_pixels) in buffer.rows().enumerate() {
+    for (count, row_pixels) in buffer.rows().enumerate() {
         for pixel in row_pixels {
             let r = pixel[0] as u32;
             let g = pixel[1] as u32;
             let b = pixel[2] as u32;
 
             let average_brightness: u32 = (r + g + b) / 3;
-            let index: usize = (average_brightness / 28) as usize;
+            let index: usize = (average_brightness / 31) as usize;
             output.push(asci_values[index]);
         }
         output.push('\n');
